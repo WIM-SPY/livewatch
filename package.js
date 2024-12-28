26b5fc625f3239dcd4601107a61063b032e146c1
Wandile Mlangeni <wandilemlangeni3@gmail.com>
2024-12-28 11:37:02 +0200

    deploy

---
+++ package.js
@@ -0,0 +1,16 @@
+{
+  "name": "live watch",
+  "version": "1.0.0",
+  "description": "",
+  "main": "wm.html",
+  "scripts": {
+    "start": "parcel wm.html --open",
+    "build": "parcel build wm.html"
+  },
+  "dependencies": {},
+  "devDependencies": {
+    "@babel/core": "7.2.0",
+    "parcel-bundler": "^1.6.1"
+  },
+  "keywords": []
+}
